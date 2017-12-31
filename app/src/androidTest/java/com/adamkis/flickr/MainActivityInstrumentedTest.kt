846ae7c62fe3358daaa1c08f9bd71961852ebdb8
adam <kisferencadam@gmail.com>
2017-12-31 12:06:46 +0100

    TestUitls and Test for clicking through navigation bar on the main activity

--- app/src/androidTest/java/com/adamkis/flickr/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/adamkis/flickr/MainActivityInstrumentedTest.kt
@@ -6,6 +6,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
@@ -25,6 +26,18 @@ class MainActivityInstrumentedTest {
 
     @Test
     fun homeActivity_Displayed() {
-        onView(withId(R.id.message)).check(matches(withText("Home")))
+        onView(withId(R.id.message)).check(matches(withText(TestUtils.getString(R.string.title_home))))
     }
+
+    @Test
+    fun homeActivity_BottomNavigationClicks(){
+        onView(withId(R.id.message)).check(matches(withText(TestUtils.getString(R.string.title_home))))
+        onView(withId(R.id.navigation_dashboard)).perform(click())
+        onView(withId(R.id.message)).check(matches(withText(TestUtils.getString(R.string.title_dashboard))))
+        onView(withId(R.id.navigation_notifications)).perform(click())
+        onView(withId(R.id.message)).check(matches(withText(TestUtils.getString(R.string.title_notifications))))
+        onView(withId(R.id.navigation_home)).perform(click())
+        onView(withId(R.id.message)).check(matches(withText(TestUtils.getString(R.string.title_home))))
+    }
+
 }
