5f57848990f493100b977163b5fc25ad77de700a
adam <kisferencadam@gmail.com>
2018-01-04 12:47:05 +0100

    testing against mock API response - mock responses are dispatched differently based on endpoints

--- app/src/androidTest/java/com/adamkis/flickr/MainActivityRESTInstrumentedTest.kt
+++ app/src/androidTest/java/com/adamkis/flickr/MainActivityRESTInstrumentedTest.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.test.InstrumentationTestCase
 import com.adamkis.flickr.network.FLICKR_URL_BASE
+import com.squareup.okhttp.mockwebserver.Dispatcher
 import com.squareup.okhttp.mockwebserver.MockResponse
 import com.squareup.okhttp.mockwebserver.MockWebServer
 import org.junit.After
@@ -17,6 +19,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import com.squareup.okhttp.mockwebserver.RecordedRequest
 
 /**
  * Created by adam on 2017. 12. 31..
@@ -50,13 +53,22 @@ class MainActivityRESTInstrumentedTest : InstrumentationTestCase() {
     @Test
     @Throws(Exception::class)
     fun homeActivity_isCorrectTitleShown() {
-        server!!.enqueue(MockResponse()
-                .setResponseCode(200)
-                .setBody(TestUtils.MOCK_RESPONSE_GETRECENT))
+        val dispatcher = object : Dispatcher() {
+            @Throws(InterruptedException::class)
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                if (request.path.startsWith("/?method=flickr.photos.getRecent")) {
+                    return MockResponse().setResponseCode(200).setBody(TestUtils.MOCK_RESPONSE_GETRECENT)
+                }
+                return MockResponse().setResponseCode(404)
+            }
+        }
+        server!!.setDispatcher(dispatcher)
+
         val intent = Intent()
         mActivityRule.launchActivity(intent)
 
         onView(withText("Pukaskwa Coastal Trail Aug-Sept 2017")).check(matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.message2)).check(matches(withText("Pukaskwa Coastal Trail Aug-Sept 2017")))
     }
 
 }
